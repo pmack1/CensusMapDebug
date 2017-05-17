2ccdff2e573c4579f54220b4b634cbd68553e538
Paul Mack <mackp@cs.uchicago.edu>
2017-05-17 17:19:10 -0500

    adding flyover for debugging

--- index.js
+++ index.js
@@ -272,3 +272,60 @@ function resetTables()
     }
   }
 };
+
+// Debug mode with Popups
+var info = L.control();
+
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info');
+  this.update();
+  return this._div;
+};
+
+info.update = function (props) {
+  this._div.innerHTML = '<h4>Census Tract</h4>' +  (props ?
+    '<b>' + props.tractce10 + '</b>'
+    : 'Hover over a tract');
+};
+info.addTo(map);
+
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+
+  info.update(layer.feature.properties);
+}
+
+var geojson;
+
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+  info.update();
+}
+
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature
+  });
+}
+
+geojson = L.geoJson(tracts, {
+  style: style,
+  onEachFeature: onEachFeature
+}).addTo(map);
